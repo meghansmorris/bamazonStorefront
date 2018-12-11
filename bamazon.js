9e7dfb71179d1c9b2b39bd2e0d13c1c32b5779e9
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-12-11 11:24:27 -0700

    creating application files

---
+++ bamazon.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+
+  database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  //runSearch();
+});
