66a602754e4b4fb27f52418ba17c65d4c618dc3b
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-12-11 13:31:29 -0700

    saving database seeds and adding code to display all products for sale

--- bamazon.js
+++ bamazon.js
@@ -12,5 +12,21 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  //runSearch();
+  readProducts();
 });
+
+//display all items for sale on bamazon
+function readProducts() {
+    console.log("Bamazon products available...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      connection.end();
+    });
+  }
+//then prompt two messages -- ID of product to buy, how many units to buy
+//then check to see if there is enough product for the customer request
+//return error if not
+//if does, fullfill request and update database with remaining quantity and show the total cost of purchase
+  //icecreamCRUD has the remove item function
--- storeSeeds.sql
+++ storeSeeds.sql
@@ -1,2 +1,9 @@
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Ficus", "Gardening", 10.00, 5);  
+VALUES ("Ficus", "Gardening", 10.00, 5), ("Garden Gnome", "Gardening", 24.99, 25),
+("Lucky Bamboo", "Gardening", 49.99, 45), 
+("Grumpy Cat 2019 Calendar", "Office Supplies", 13.50, 100), 
+("Set of 10 Purple Pens", "Office Supplies", 5.99, 450), 
+("Slinky", "Toys", 12.99, 167), ("Troll Doll", "Toys", 8.00, 48), 
+("Cat Lover Socks", "Gifts", 14.50, 575), ("Ukele", "Musical Instruments", 175.00, 12), 
+("Accordian", "Musical Instruments", 299.99, 3), ("Roomba Vacuum", "Gifts", 199.99, 134), 
+("Yoga Mat", "Gifts", 80.00, 375);
